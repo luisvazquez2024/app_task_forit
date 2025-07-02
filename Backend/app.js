63b7ae9c8b8c3ff9757bb71eea61caca8d41ab79
luisvazquez2024 <luisvazquezcp.3000@gmail.com>
2025-07-01 23:01:11 -0300

    se agrego variables de entorno y cors al backend

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require('dotenv').config();
 
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+
+app.use(cors());
 app.use(express.json())
 
 let tasks = [];
-
-
-
 
 
 app.get('/', (req, res) => {
